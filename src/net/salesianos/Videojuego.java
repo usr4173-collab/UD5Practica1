87ee6bbcd1955ff6e00f8f03564a4e33d2c71f3d
usr4173-collab <usr4173@salesianos-lacuesta.net>
2026-05-17 18:42:32 +0100

    Añadidos getters y setters de la clase videojuego.

--- src/net/salesianos/Videojuego.java
+++ src/net/salesianos/Videojuego.java
@@ -7,5 +7,45 @@ public class Videojuego {
     private String plataforma;
     private int puntuacion;
 
-    
+    public Videojuego() {}
+
+    public Videojuego(String nombre, String genero, String plataforma, int puntuacion) {
+        this.nombre = nombre;
+        this.genero = genero;
+        this.plataforma = plataforma;
+        this.puntuacion = puntuacion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setPlataforma(String plataforma) {
+        this.plataforma = plataforma;
+    }
+
+    public String getPlataforma() {
+        return plataforma;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
 }
