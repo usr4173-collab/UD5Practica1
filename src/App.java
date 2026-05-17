20460fd7f0684f003952d4524670c1ddf2bba046
usr4173-collab <usr4173@salesianos-lacuesta.net>
2026-05-17 19:13:11 +0100

    He comentado la parte del menú para probar si se imprimía correctamente los datos del videojuego.

--- src/App.java
+++ src/App.java
@@ -3,13 +3,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    
+
     static ArrayList<Videojuego> listaVideojuegos = new ArrayList<>();
+
     public static void main(String[] args) throws Exception {
-        
+
         Scanner usuario = new Scanner(System.in);
 
-        System.out.println("Vamos a buscar un videojuego, ");
+        /*System.out.println("Vamos a buscar un videojuego, ");
         System.out.println("introduce el nombre: ");
         String nombre = usuario.nextLine();
 
@@ -20,9 +21,14 @@ public class App {
         String plataforma = usuario.nextLine();
 
         System.out.println("Introduce la puntuación: ");
-        String puntuacion = usuario.nextLine();
-        
+        String puntuacion = usuario.nextLine();*/
+
+        Videojuego videojuego1 = new Videojuego("The Last Of Us II", "Terror de supervivencia", "PS4", 9);
+        listaVideojuegos.add(videojuego1);
+
+        for (Videojuego v : listaVideojuegos) {
+            System.out.println(v);
+        }
     }
-    
-    
+
 }
