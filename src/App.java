1ed81f0d902b8141987948a2e725a71336aa6373
usr4173-collab <usr4173@salesianos-lacuesta.net>
2026-05-17 19:05:37 +0100

    He creado un menú que pide diferentes datos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import net.salesianos.Videojuego;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+    
+    static ArrayList<Videojuego> listaVideojuegos = new ArrayList<>();
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner usuario = new Scanner(System.in);
+
+        System.out.println("Vamos a buscar un videojuego, ");
+        System.out.println("introduce el nombre: ");
+        String nombre = usuario.nextLine();
+
+        System.out.println("Introduce el género: ");
+        String genero = usuario.nextLine();
+
+        System.out.println("Introduce la plataforma: ");
+        String plataforma = usuario.nextLine();
+
+        System.out.println("Introduce la puntuación: ");
+        String puntuacion = usuario.nextLine();
+        
     }
+    
+    
 }
